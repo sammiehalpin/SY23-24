82b45fd3e97266b358b166263fe5fdb2ccf6811e
Sammie <sahal625@learner.dvusd.org>
2024-01-08 14:01:55 -0700

    Added Menu

--- Menus/Menus/Form1.vb
+++ Menus/Menus/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
+    Private Sub ClothesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClothesToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        TextBox1.Clear()
+    End Sub
+
 
 End Class
